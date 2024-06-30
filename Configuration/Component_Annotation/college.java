bdb23f826a64af473acdabe995ede668f755c07f
sheetal gupta <134307985+sheetal-81@users.noreply.github.com>
2024-06-30 18:46:34 +0530

    Create college.java

---
+++ Configuration/Component_Annotation/college.java
@@ -0,0 +1,13 @@
+package com.example.demo.Component_Annotation;
+
+public class college {
+            public void test(){
+            	System.out.println("Hello World");
+                        System.out.println("printing the college file content");
+                        int a=5;
+                        int b=10;
+                        int sum=a+b;
+                        System.out.println(sum);
+            }
+            
+}
